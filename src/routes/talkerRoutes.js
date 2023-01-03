7e70aa85f1e5703731f82ba847aebb5565fb59e4
Kleber <kleberst88@gmail.com>
2023-01-03 18:19:38 -0300

    feat: Requisito 07 feito

--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -65,6 +65,15 @@ talkerRouter.put('/talker/:id',
     return res.status(200).json(talkerUpdated);
 });
 
+talkerRouter.delete('/talker/:id', authMiddleware, async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readFile();
+  const talkerUpdated = talkers.map((el) => el.id !== id);
+
+  await writeFile(talkerUpdated);
+  return res.status(204).json();
+});
+
 talkerRouter.use((error, req, res, _next) => {
   res.status(error.status || 500).json({ message: error.message });
 });
